eec394394857e97e722db6f21e462381a200a74a
Shan <stwm2@student.london.ac.uk>
2021-01-13 02:45:59 +0800

    Bug fixes, added requirements file

---
+++ run.py
@@ -0,0 +1,39 @@
+import argparse, sys
+import json
+
+from Bio.SeqIO import parse
+from window import SlidingWindow
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='Absolute path to the aligned sequences file in FASTA format.')
+parser.add_argument('-o', '--output', help='Absolute path to the output file.')
+parser.add_argument('-l', '--length', help='The k-mer length (default: 9)', type=int, default=9, choices=range(1, 15))
+
+arguments = parser.parse_args()
+
+if not arguments.input or not arguments.output:
+    print('Error: The input and output path names are not provided.')
+    sys.exit(2)
+
+try:
+    sequences = open(arguments.input, 'r')
+except FileNotFoundError as err:
+    print(str(err.strerror))
+    sys.exit(3)
+
+try:
+    parsed_sequences = parse(sequences, 'fasta')
+except Exception:
+    print(f'Unknown exception while parsing the provided input file {arguments.input}')
+    sys.exit(4)
+
+try:
+    kmer_positions = SlidingWindow(parsed_sequences, arguments.length).run()
+except Exception:
+    print(f'Exception while calculating kmers for sequences file {arguments.input}')
+    sys.exit(5)
+
+with open(arguments.output, 'w') as output:
+    output.write(json.dumps(list(kmer_positions), indent=2))
+
+print(f'Results successfully saved at {arguments.output}')
---
+++ window.py
@@ -0,0 +1,110 @@
+from collections import Counter
+from itertools import islice
+
+from datatypes import Position, Variant, VariantDict
+
+
+class SlidingWindow(object):
+    """
+        Sliding Window Class
+
+        Gets a zip object containing k-mers from from all the sequences provided classified by position.
+
+        Example:
+            >>> from window import SlidingWindow
+            >>> window = SlidingWindow(iter[SeqRecord], 9)
+            >>> kmers = window.kmers()
+            >>> kmer1 = list(kmers[0]) # First k-mer of all sequences
+            >>> kmer2 = list(kmers[1]) # Second k-mer of all sequences
+    """
+
+    DISALLOWED_CHARS = {'-', 'X', 'B', 'J', 'Z', 'O', 'U'}
+
+    def __init__(self, seqs, kmer_len=9):
+        """
+            Args:
+                seqs: An iterable of type SeqRecord.
+                kmer_len: The length of the sliding window (Default: 9).
+        """
+
+        self.seqs = list(seqs)
+        self.kmer_len = kmer_len
+
+    def _create_variant_dict(self, kmers):
+        positions = []
+
+        for kmer in kmers:
+            unique = VariantDict(list)
+            # Here there's no need to enumerate, you can just append any value because you are essentialy
+            # only using len to get the number of elements. There has to be a more elegant way to achive the same effect
+            for a, b in enumerate(kmer):
+                unique[b].append(a)
+            unique.pop('illegal-char', None)
+            if len(unique) > 0:
+                positions.append(unique)
+        return positions
+
+    def _kmers(self):
+        """
+            Extracts k-mers from all the sequences provided.
+
+            Returns:
+                Zip: A Zip object containing all k-mers.
+        """
+
+        seqs = map(lambda s: str(s.seq), self.seqs)
+        kmers_seqs = map(lambda s: self._window(s, self.kmer_len), seqs)
+        return zip(*kmers_seqs)
+
+    def _window(self, seq, kmer_len):
+        """
+            The actual logic of sliding window.
+
+            Returns:
+                A Generator object for all kmers within the alignment
+        """
+
+        it = iter(seq)
+        result = ''.join(islice(it, kmer_len))
+        if len(result) == kmer_len:
+            if all(ele not in result for ele in self.DISALLOWED_CHARS):
+                yield result
+            else:
+                yield 'illegal-char'
+
+        for elem in it:
+            result = result[1:] + elem
+            if all(ele not in result for ele in self.DISALLOWED_CHARS):
+                yield result
+            else:
+                yield 'illegal-char'
+
+    def _create_position_objects(self, counters):
+        """
+            Create kmer position objects with entropy, position and variant properties
+
+            Returns:
+                A Generator object for Position objects for each kmer position
+        """
+
+        for idx, counter in enumerate(counters, start=1):
+            yield Position(
+                position=idx,
+                sequences=self._create_variant_objects(idx, counter)
+            )
+
+    def _create_variant_objects(self, idx, variant_counter: Counter) -> list:
+        variant_objects = [
+            Variant(idx, seq, count)
+            for seq, count
+            in variant_counter.items()
+        ]
+
+        return variant_objects
+
+    def run(self):
+        kmers = self._kmers()
+        variant_dicts = self._create_variant_dict(kmers)
+        variant_counters = [x.get_counter() for x in variant_dicts]
+        kmer_position_objects = self._create_position_objects(variant_counters)
+        return kmer_position_objects
