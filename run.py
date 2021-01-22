4d0c4e4061f1d6ff5b5b5b9e442538a34fb41225
Shan <stwm2@student.london.ac.uk>
2021-01-22 18:09:28 +0800

    Now supports writing to stdout if no output file is specified. All errors are written to stderr by default.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse, sys
 import json
+import sys
 
 from Bio.SeqIO import parse
 from window import SlidingWindow
@@ -11,30 +12,36 @@ parser.add_argument('-l', '--length', help='The k-mer length (default: 9)', type
 
 arguments = parser.parse_args()
 
-if not arguments.input or not arguments.output:
-    print('Error: The input and output path names are not provided.')
+if not arguments.input:
+    print('Error: The input path is not provided.', file=sys.stderr)
     sys.exit(2)
 
 try:
     sequences = open(arguments.input, 'r')
 except FileNotFoundError as err:
-    print(str(err.strerror))
+    print(str(err.strerror), file=sys.stderr)
     sys.exit(3)
 
 try:
     parsed_sequences = parse(sequences, 'fasta')
 except Exception:
-    print(f'Unknown exception while parsing the provided input file {arguments.input}')
+    print(f'Unknown exception while parsing the provided input file {arguments.input}', file=sys.stderr)
     sys.exit(4)
 
 try:
     kmer_positions = SlidingWindow(parsed_sequences, arguments.length).run()
     sequences.close()
 except Exception:
-    print(f'Exception while calculating kmers for sequences file {arguments.input}')
+    print(f'Exception while calculating kmers for sequences file {arguments.input}', file=sys.stderr)
     sys.exit(5)
 
+json_results = json.dumps(list(kmer_positions))
+
+if not arguments.output:
+    print(json_results)
+    sys.exit(0)
+
 with open(arguments.output, 'w') as output:
-    output.write(json.dumps(list(kmer_positions), indent=2))
+    output.write(json.dumps(json_results, indent=2))
 
 print(f'Results successfully saved at {arguments.output}')
